287380e7e4496f18781126ce23733fae930f047d
Thibault <thibault.pras@epitech.eu>
2024-07-15 20:22:23 +0200

    package: added react-router for navigation

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,20 @@ import { App } from './App.tsx'
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <RouterProvider router={router} />
     </MantineProvider>
   </React.StrictMode>,
 )
